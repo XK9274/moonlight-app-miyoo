ba958dd605c202a0f42c153d85a6c4d28928f08b
XK <47260768+XK9274@users.noreply.github.com>
2023-10-22 09:52:17 +0100

    Move/Remove files

--- moonlight/script/launch.sh
+++ moonlight/script/launch.sh
@@ -6,52 +6,67 @@ export miyoodir=/mnt/SDCARD/miyoo
 cd $moonlightdir
 export PATH=$PATH:$PWD/bin
 export HOME=$moonlightdir
-rm -rf /tmp/st_exit
+
+get_curvol() {
+    awk '/LineOut/ {if (!printed) {gsub(",", "", $8); print $8; printed=1}}' /proc/mi_modules/mi_ao/mi_ao0
+}
+
+kill_audio_servers() {
+    is_process_running "audioserver" && pkill -9 -f "audioserver"
+    is_process_running "audioserver.mod" && killall -q "audioserver.mod"
+}
+
+set_snd_level() {
+    local target_vol="$1"
+    local current_vol
+    local start_time
+    local elapsed_time
+
+    start_time=$(date +%s)
+    while [ ! -e /proc/mi_modules/mi_ao/mi_ao0 ]; do
+        sleep 0.2
+        elapsed_time=$(( $(date +%s) - start_time ))
+        if [ "$elapsed_time" -ge 30 ]; then
+            echo "Timed out waiting for /proc/mi_modules/mi_ao/mi_ao0"
+            return 1
+        fi
+    done
+
+    start_time=$(date +%s)
+    while true; do
+        echo "set_ao_volume 0 ${target_vol}dB" > /proc/mi_modules/mi_ao/mi_ao0
+        echo "set_ao_volume 1 ${target_vol}dB" > /proc/mi_modules/mi_ao/mi_ao0
+        current_vol=$(get_curvol)
+
+        if [ "$current_vol" = "$target_vol" ]; then
+            echo "Volume set to ${current_vol}dB"
+            return 0
+        fi
+
+        elapsed_time=$(( $(date +%s) - start_time ))
+        if [ "$elapsed_time" -ge 360 ]; then
+            echo "Timed out trying to set volume"
+            return 1
+        fi
+
+        sleep 0.2
+    done
+}
 
 start_moonlight() {
     export LD_LIBRARY_PATH=../lib:/lib:/config/lib:/mnt/SDCARD/miyoo/lib:/mnt/SDCARD/.tmp_update/lib:/mnt/SDCARD/.tmp_update/lib/parasyte:/sbin:/usr/sbin:/bin:/usr/bin
-    (rapid-splash $moonlightdir/splash 0 0 0 50 & sleep 10; killall -9 rapid-splash) &
     export SDL_VIDEODRIVER=mmiyoo
     export SDL_AUDIODRIVER=mmiyoo
     export EGL_VIDEODRIVER=mmiyoo
     
     kill_audio_servers
-      
-    monitor_output &
     set_snd_level "${curvol}" &
-    script -c "LD_PRELOAD=$moonlightdir/lib/libuuid.so moonlight -config ./config/moonlight.conf stream" /tmp/output
+    LD_PRELOAD=$moonlightdir/lib/libuuid.so moonlight -config ./config/moonlight.conf stream
 }
 
 main() {
-    . $moonlightdir/script/util.sh    
-    save_fb_properties # to restore framebuffer properties on the way out, stops black screens leaving back to mainui
     curvol=$(get_curvol) # grab current volume
-    touch /tmp/output 
-
-    if [ -f $moonlightdir/config/pairdone ]; then
-        get_user_app
-        if [ -f /tmp/launch ]; then
-            start_moonlight
-            do_cleanup
-        else
-            do_cleanup
-        fi
-    else
-        $moonlightdir/bin/pressMenu2Term neo-st &
-        $moonlightdir/bin/neo-st -e $moonlightdir/script/pair.sh
-        if [ -d "$moonlightdir/.cache" ] && [ -f "$moonlightdir/config/pairdone" ]; then # second catch for this file
-            get_user_app
-            start_moonlight
-            do_cleanup
-        fi
-    fi
-
-    if pgrep "rapid-splash" > /dev/null; then
-      sleep 5 && killall -9 rapid-splash
-    else
-      echo "rapid-splash is not running."
-    fi
-    do_cleanup
+    start_moonlight
 }
 
 main
